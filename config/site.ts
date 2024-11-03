f8378c9a47b565eb5992907ef38ed93e81ba9a72
SandeepGarikapati <sandeepgarikapati17@gmail.com>
2024-11-03 18:23:25 +0530

    Commit added

--- config/site.ts
+++ config/site.ts
@@ -1,12 +1,12 @@
 export const siteConfig = {
-  name: "JollyBlog",
+  name: "Sandeep",
   url: "https://example.com",
   description: "Nextjs 14 blog using velite, tailwind and shadcn",
-  author: "JollyCoding",
+  author: "Sandeep Garikapati",
   links: {
-    twitter: "https://twitter.com/jollyshopland",
-    github: "https://github.com/jolbol1",
-    personalSite: "https://jamesshopland.com",
+    twitter: "#",
+    github: "https://github.com/SandeepGarikapati",
+    personalSite: "https://www.sandeepgarikapati.tech/",
   },
 };
 
